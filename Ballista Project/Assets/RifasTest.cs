8780995661b15fec5fddc368864c7cd58d5dccfd
RifasRepo <56219263+RifasRepo@users.noreply.github.com>
2021-02-23 18:11:47 +0000

    mapvs1

---
+++ Ballista Project/Assets/RifasTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RifasTest : MonoBehaviour
+{
+    public GameObject NPC, Player, npcver;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+        Player.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(npcver.transform.position, Player.transform.position) < 170)
+        {
+            NPC.SetActive(true);
+        }
+        else
+        {
+            NPC.SetActive(false);
+        }
+      
+    }
+}
