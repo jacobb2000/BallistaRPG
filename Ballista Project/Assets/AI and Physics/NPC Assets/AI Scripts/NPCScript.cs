17cd74131e4b24572777c93e3f67e164f79c3490
JordanLW99 <WindsorJordan1999@hotmail.co.uk>
2021-02-23 15:58:56 +0000

    AI rendering

--- Ballista Project/Assets/AI and Physics/NPC Assets/AI Scripts/NPCScript.cs
+++ Ballista Project/Assets/AI and Physics/NPC Assets/AI Scripts/NPCScript.cs
@@ -24,6 +24,7 @@ public class NPCScript : MonoBehaviour
         //i = Random.Range(0, nodes.nodes.Length);
     }
 
+
     public float timer;
 
     private void Moving()
---
+++ Ballista Project/Assets/AI and Physics/NPC Assets/AI Scripts/RenderDistanceAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderDistanceAI : MonoBehaviour
+{
+    public GameObject NPC;
+    public GameObject NPCParent;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        NPC = gameObject.transform.GetChild(0).gameObject;
+        NPCParent = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        float distancetoplayer = Vector3.Distance(Player.transform.position, NPC.transform.position);
+
+        if (distancetoplayer < 20)
+        {
+            NPC.SetActive(false);
+        }
+        if (distancetoplayer > 20)
+        {
+            NPC.SetActive(true);
+        }
+    }
+}
